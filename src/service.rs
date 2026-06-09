c01d4bf93f7066455e47f01e4f8f31b633c8466c
luoda2024 <info@dicad.cn>
2026-06-09 17:01:39 +0800

    LUODA Remote Desktop v2.0.0 - 归档版

---
+++ src/service.rs
@@ -0,0 +1,22 @@
+use luoda::*;
+
+
+
+#[cfg(not(target_os = "macos"))]
+
+fn main() {}
+
+
+
+#[cfg(target_os = "macos")]
+
+fn main() {
+
+    crate::common::load_custom_client();
+
+    hbb_common::init_log(false, "service");
+
+    crate::start_os_service();
+
+}
+
